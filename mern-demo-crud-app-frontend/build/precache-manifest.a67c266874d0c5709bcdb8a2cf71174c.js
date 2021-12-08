0cddfd5a2ff24ffbb676ea044a90300880e40b53
vinay-14298 <vtu14298@veltech.edu.in>
2021-12-08 11:46:20 +0530

    Added build folder

---
+++ mern-demo-crud-app-frontend/build/precache-manifest.a67c266874d0c5709bcdb8a2cf71174c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32289bd024190e9843f4df44cb833a27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb577a9aba210a01a36d",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "6cb1aa878949894349d8",
+    "url": "/static/css/main.9b05a1ca.chunk.css"
+  },
+  {
+    "revision": "bb577a9aba210a01a36d",
+    "url": "/static/js/2.cdac6e34.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cdac6e34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cb1aa878949894349d8",
+    "url": "/static/js/main.e1e6e500.chunk.js"
+  },
+  {
+    "revision": "1af47c5e440203105a31",
+    "url": "/static/js/runtime-main.66ee3d9b.js"
+  }
+]);
